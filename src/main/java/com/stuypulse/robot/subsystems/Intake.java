bb8df4044260ef4265df87ee53653d1448707916
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-12-02 00:19:44 -0500

    Add intake imports

--- src/main/java/com/stuypulse/robot/subsystems/Intake.java
+++ src/main/java/com/stuypulse/robot/subsystems/Intake.java
@@ -3,6 +3,8 @@ package com.stuypulse.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
